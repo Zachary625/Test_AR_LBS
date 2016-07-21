f190c2fc337f1ec653cb4314f93ec6bfaa089e81
Zachary625 <Zachary625@outlook.com>
2016-07-21 20:40:32 +0800

    测试陀螺仪代码初步

--- Assets/Scripts/VirtualCamera/VirtualCameraController.cs
+++ Assets/Scripts/VirtualCamera/VirtualCameraController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class VirtualCameraController : MonoBehaviour {
 
+	private Gyroscope gyroscope;
+
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log (" @ VirtualCameraController.Start(): SystemInfo.supportsGyroscope: " + SystemInfo.supportsGyroscope);
+		if (SystemInfo.supportsGyroscope) {
+			this.gyroscope = Input.gyro;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this._readGyroscope ();
+	}
+
+	void _readGyroscope() {
+		if (this.gyroscope == null) {
+			return;
+		}
+
+		Debug.Log (" @ VirtualCameraController._readGyroscope(): " + this.gyroscope.attitude.ToString());
+		this.gameObject.transform.rotation = this.gyroscope.attitude;
 	}
 }
