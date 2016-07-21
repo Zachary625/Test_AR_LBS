a1df3ecd7a4edace355d06336ead473178e37573
Zachary625 <Zachary625@outlook.com>
2016-07-21 21:04:04 +0800

    加入GPS

--- Assets/Scripts/VirtualCamera/VirtualCameraController.cs
+++ Assets/Scripts/VirtualCamera/VirtualCameraController.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class VirtualCameraController : MonoBehaviour {
 
 	private Gyroscope gyroscope;
+	private LocationService gps;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log (" @ VirtualCameraController.Start(): SystemInfo.supportsGyroscope: " + SystemInfo.supportsGyroscope);
+		Debug.Log (" @ VirtualCameraController.Start(): SystemInfo.supportsLocationService: " + SystemInfo.supportsLocationService);
 		if (SystemInfo.supportsGyroscope) {
 			this.gyroscope = Input.gyro;
+		}
+		if (SystemInfo.supportsLocationService) {
+			this.gps = Input.location;
+			this.gps.Start ();
+		}
+	}
+
+	void Stop() {
+		if (this.gps != null) {
+			this.gps.Stop ();
 		}
 	}
 	
@@ -23,7 +35,6 @@ public class VirtualCameraController : MonoBehaviour {
 			return;
 		}
 
-		Debug.Log (" @ VirtualCameraController._readGyroscope(): " + this.gyroscope.attitude.ToString());
 		this.gameObject.transform.rotation = this.gyroscope.attitude;
 	}
 }
