4c9ceb7f1659e216500308b598ce36cc2b0a1107
Zachary <zachary625@outlook.com>
2016-07-25 01:19:30 +0800

    继续尝试
    考虑用陀螺仪和指南针的三维向量结合相机进行控制

--- Assets/Scripts/VirtualCamera/VirtualCameraController.cs
+++ Assets/Scripts/VirtualCamera/VirtualCameraController.cs
@@ -9,6 +9,7 @@ public class VirtualCameraController : MonoBehaviour {
 
 	private LocationInfo location;
 	private float compassTrueHeading;
+    private Vector3 compassRawVector;
 
 	private Quaternion baseRotation = Quaternion.Euler(new Vector3(90, 180, 0));
     private Quaternion fixRotation = new Quaternion(0,0,1,0);
@@ -16,17 +17,12 @@ public class VirtualCameraController : MonoBehaviour {
 	private Vector3 gyroGravity;
     private Quaternion cameraRotation;
 
-	private bool _reverseX;
-	private bool _reverseY;
-	private bool _reverseZ;
-	private bool _reverseRotation;
-	private bool _compassAndGravity;
-
     private enum _RotationMethod
     {
         Attitude,
 		GravityAndHeading1,
 		GravityAndHeading2,
+        GravityAndRawVector,
 		Length,
     }
 
@@ -97,16 +93,11 @@ public class VirtualCameraController : MonoBehaviour {
             this.rotationMethod = (_RotationMethod)(((int)(this.rotationMethod + 1)) % ((int)(_RotationMethod.Length)));
 		}
 
-		this._reverseX = GUI.Toggle (new Rect (0, height * (row++), 200, height), this._reverseX, "reverse x");
-		this._reverseY = GUI.Toggle (new Rect (0, height * (row++), 200, height), this._reverseY, "reverse y");
-		this._reverseZ = GUI.Toggle (new Rect (0, height * (row++), 200, height), this._reverseZ, "reverse z");
-		this._reverseRotation = GUI.Toggle (new Rect (0, height * (row++), 200, height), this._reverseRotation, "reverse rotation");
-		this._compassAndGravity = GUI.Toggle (new Rect (0, height * (row++), 200, height), this._compassAndGravity, "compass * gravity");
-
 		GUI.Label (new Rect (0, height *(row++), 500, height), "screen orientation: " + this._getScreenOrientationString());
 		GUI.Label (new Rect (0, height *(row++), 500, height), "compassTrueHeading: " + this.compassTrueHeading);
+        GUI.Label(new Rect(0, height * (row++), 500, height), "compassRawVector: " + this.compassRawVector);
 
-		GUI.Label (new Rect (0, height *(row++), 500, height), "gyroAttitude: " + this._quaternionToString(this.gyroAttitude));
+        GUI.Label (new Rect (0, height *(row++), 500, height), "gyroAttitude: " + this._quaternionToString(this.gyroAttitude));
 		GUI.Label (new Rect (0, height *(row++), 500, height), "gyroGravity: " + (this.gyroGravity.ToString()));
 
 		GUI.Label (new Rect (0, height *(row++), 500, height), "cameraRotation: " + this._quaternionToString(this.cameraRotation));
@@ -125,6 +116,7 @@ public class VirtualCameraController : MonoBehaviour {
 		}
 		if (this.compass != null) {
 			this.compassTrueHeading = this.compass.trueHeading;
+            this.compassRawVector = this.compass.rawVector;
 		}
 
 		switch (this.rotationMethod) {
@@ -134,30 +126,12 @@ public class VirtualCameraController : MonoBehaviour {
 				this.transform.localRotation = this.cameraRotation;
 				break;
 			}
-			break;
-		case _RotationMethod.GravityAndHeading1: 
-			{
-				Quaternion compassRotation = Quaternion.AngleAxis (this.compassTrueHeading, Vector3.up);
-
-				if (this._reverseX) {
-					this.gyroGravity.x *= -1;
-				}
-				if (this._reverseY) {
-					this.gyroGravity.y *= -1;
-				}
-				if (this._reverseZ) {
-					this.gyroGravity.z *= -1;
-				}
-				Quaternion gravityRotation = this._reverseRotation? Quaternion.FromToRotation(this.gyroGravity, Vector3.down): Quaternion.FromToRotation (Vector3.down, this.gyroGravity);
-				this.cameraRotation = this._compassAndGravity? compassRotation * gravityRotation: gravityRotation * compassRotation;
-				this.transform.localRotation = this.cameraRotation;
-
-				break;
-			}
-		case _RotationMethod.GravityAndHeading2:
-			{
-				break;
-			}
+        case _RotationMethod.GravityAndRawVector:
+            {
+                this.cameraRotation = Quaternion.LookRotation(-this.compassRawVector, -this.gyroGravity);
+                this.transform.localRotation = this.cameraRotation;
+                break;
+            }
 		}
 	}
 
